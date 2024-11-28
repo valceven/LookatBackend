789bdd8a1ac105e553915cb3795e86e56bbea9b3
Val Mykel Ceven Bolante <valmykelcevenbolante@gmail.com>
2024-11-29 01:27:39 +0800

    Revert "For the shark pull badge "

--- LookatBackend/Dtos/User/CreateUserRequestDto.cs
+++ LookatBackend/Dtos/User/CreateUserRequestDto.cs
@@ -4,19 +4,19 @@ namespace LookatBackend.Dtos.User
 {
     public class CreateUserRequestDto
     {
-        public string? UserName { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Password { get; set; }
-        public string? MobileNumber { get; set; } // Changed to string
-        public DateTime? Date { get; set; }
-        public string? PhysicalIdNumber { get; set; }
-        public string? Purok { get; set; }
-        public string? BarangayLoc { get; set; }
-        public string? CityMunicipality { get; set; }
-        public string? Province { get; set; }
-        public string? FullAddress => $"{Purok}, {BarangayLoc}, {CityMunicipality}, {Province}";
-        public string? Email { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string MobileNumber { get; set; } // Changed to string
+        public DateTime Date { get; set; }
+        public string PhysicalIdNumber { get; set; }
+        public string Purok { get; set; }
+        public string BarangayLoc { get; set; }
+        public string CityMunicipality { get; set; }
+        public string Province { get; set; }
+        public string FullAddress => $"{Purok}, {BarangayLoc}, {CityMunicipality}, {Province}";
+        public string Email { get; set; }
         // commnetd because not so sure pa public IFormFile ProfilePicture { get; set; }
         // also need mo verify para naay barngay public string? BarangayId { get; set; } // Made nullable
         //public Barangay Barangay { get; set; }
